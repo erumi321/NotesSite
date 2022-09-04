1c03ae340e0eb5a26c5e2fb44ca9142ea22ae97b
erumi321 <42208651+erumi321@users.noreply.github.com>
2022-09-04 18:19:40 -0400

    inital document writing

--- googleAPI.js
+++ googleAPI.js
@@ -22,76 +22,132 @@ function revokeToken() {
     google.accounts.oauth2.revoke(access_token, () => {console.log('access token revoked')});
 }
 
-function getThumbnail(file, API_KEY, index) {
-    var imageXHR = new XMLHttpRequest();
-    imageXHR.open('GET', "https://www.googleapis.com/drive/v3/files/" + file.id + "?fields=thumbnailLink&key=" + API_KEY);
-    imageXHR.setRequestHeader('Authorization', 'Bearer ' + access_token);
-    
-    imageXHR.onreadystatechange = () => {
-        if (imageXHR.readyState === 4) {
-            var imageElement = document.getElementById('sidebarImage' + index);
-            console.log(imageXHR.response);
-            console.log(imageElement);
-            console.log(JSON.parse(imageXHR.response));
-            console.log(JSON.parse(imageXHR.response).thumbnailLink);
-            if (JSON.parse(imageXHR.response).thumbnailLink != null){
-
-            imageElement.setAttribute('src', JSON.parse(imageXHR.response).thumbnailLink); 
-        }
-        }
-    }
-    imageXHR.send();
-}
-
-function loadCalendar() {
+function loadDrivePages(pageToken) {
     var xhr = new XMLHttpRequest();
-
     requestURLParameters = [
         'orderBy', 'modifiedTime%20desc',
-        'fields', 'files(id,name,hasThumbnail,thumbnailLink,iconLink)',
+        'fields', 'nextPageToken,files(id,name,mimeType)',
+        'trashed', false,
         'key', API_KEY
     ]
+
+    if (pageToken != null) {
+        requestURLParameters.splice(0, 0, pageToken);
+        requestURLParameters.splice(0, 0, 'pageToken');
+    }
 
     requestURL = 'https://www.googleapis.com/drive/v3/files?'
     for(var i = 0; i < requestURLParameters.length; i+=2) {
         requestURL += requestURLParameters[i] + "=" + requestURLParameters[i + 1] + "&";
     }
 
-    requestURL = requestURL.slice(0, -1);
-    console.log(requestURL);
-
     xhr.open('GET', requestURL);
     xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
     xhr.setRequestHeader('Accept', 'json');
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
-            xhrJson = JSON.parse(xhr.response);
+            var xhrJson = JSON.parse(xhr.response);
             
             cardContainer = document.getElementById("context-bar").children[1];
             
             for(var i = 0; i < xhrJson.files.length; i++){
                 var file = xhrJson.files[i];
                 
+                //only allow google doc files
+                if (file.mimeType != "application/vnd.google-apps.document") {
+                    continue;
+                }
+
                 var container = document.createElement('div');
                 container.classList.add('context-card');
 
-                var image = document.createElement('img');
-                image.setAttribute('id', 'sidebarImage' + i);
-
-                //image.setAttribute('src', file.iconLink);
-                image.classList.add('context-card-image');
-
-                container.appendChild(image);
-
-                getThumbnail(file, API_KEY, i);
-
                 var text = document.createElement('p');
-                text.innerText = xhrJson.files[i].name;
+                text.innerText = xhrJson.files[i].name + " : " + file.id;
 
                 container.appendChild(text);
                 cardContainer.appendChild(container);
             }
+
+            if (xhrJson.nextPageToken) {
+                console.log(xhrJson.nextPageToken);
+                loadDrivePages(xhrJson.nextPageToken);
+            }
         }
     }
     xhr.send();
+} 
+
+function sendBatchUpdate(id, requestData, callback){
+    var postXhr = new XMLHttpRequest();
+
+    postXhr.open('POST', "https://docs.googleapis.com/v1/documents/" + id + ":batchUpdate?key=" + API_KEY);
+
+    postXhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    postXhr.setRequestHeader('Accept', 'application/json');
+    postXhr.setRequestHeader('Content-Type', 'application/json');
+    postXhr.onreadystatechange = () => {
+        if (postXhr.readyState === 4) {
+            callback(postXhr.response);
+        }
+    }
+
+    postXhr.send(requestData);
+}
+
+function clearDriveDoc(id, callback) {
+    //test doc id: 1EqWilimO1NDqgoJ3zzUxW4c5NcATWxVNX4y2_jlz8uM
+
+    var getXhr = new XMLHttpRequest();
+
+    getXhr.open('GET', 'https://docs.googleapis.com/v1/documents/' + id + '?fields=body.content(startIndex%2CendIndex)&key=' + API_KEY);
+    getXhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    getXhr.setRequestHeader('Accept', 'application/json');
+    getXhr.onreadystatechange = () => {
+        if (getXhr.readyState === 4) {
+            var xhrJSON = JSON.parse(getXhr.response);
+
+            var content = xhrJSON.body.content;
+
+            var startIndex = content[1].startIndex;
+            var endIndex = content[content.length - 1].endIndex - 1;
+
+            if (endIndex == 1) {
+                callback();
+                return;
+            }
+
+            var requestData = {
+                "requests": [
+                  {
+                    "deleteContentRange": {
+                      "range": {
+                        "startIndex": startIndex,
+                        "endIndex": endIndex
+                      }
+                    }
+                  }
+                ]
+              }
+
+            sendBatchUpdate(id, JSON.stringify(requestData).replace(/\\"/g, '"'), callback);
+        }
+    }
+
+    getXhr.send();
+}
+
+function writeDriveDoc(id, startLocation = 1, content, callback) {
+    var requestData = {
+        "requests": [
+          {
+            "insertText": {
+              "location": {
+                "index": startLocation
+              },
+              "text": content
+            }
+          }
+        ]
+      }
+    sendBatchUpdate(id, JSON.stringify(requestData).replace(/\\"/g, '"'), callback);
 }
--- index.js
+++ index.js
@@ -3,3 +3,13 @@ document.getElementById("main-text").addEventListener('input', function() {
 
 	document.getElementById("main-text").style.height = scroll_height + 'px';
 });
+
+function SaveDoc(id) {
+    id = "1EqWilimO1NDqgoJ3zzUxW4c5NcATWxVNX4y2_jlz8uM";
+
+	console.log(document.getElementById('main-text').value);
+
+    clearDriveDoc(id, () => {
+        writeDriveDoc(id, 1, document.getElementById('main-text').value, () => {console.log("YAHOOO, written")});
+    })
+}
